fbce64fd8535624ba0600d6948413e811029dd72
Ayfri <pierre.ayfri@gmail.com>
2023-05-24 17:15:10 +0200

    chore(back): Add SQL libraries.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,11 +1,13 @@
-val ktorVersion = project.ext["ktor.version"] as String
 val logbackVersion = project.ext["logback.version"] as String
+val komapperVersion = project.ext["komapper.version"] as String
+val ktorVersion = project.ext["ktor.version"] as String
 
 plugins {
+    application
+    id("com.google.devtools.ksp") version "1.8.20-1.0.10"
+    id("io.ktor.plugin")
     kotlin("jvm")
     kotlin("plugin.serialization")
-    id("io.ktor.plugin")
-    application
 }
 
 repositories {
@@ -28,4 +30,11 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-cio-jvm:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    platform("org.komapper:komapper-platform:$komapperVersion").let {
+        implementation(it)
+        ksp(it)
+    }
+    implementation("org.komapper:komapper-starter-jdbc")
+    ksp("org.komapper:komapper-processor")
 }
--- backend/src/main/kotlin/io/github/aifiltration/Application.kt
+++ backend/src/main/kotlin/io/github/aifiltration/Application.kt
@@ -5,16 +5,12 @@ import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 import io.github.aifiltration.plugins.configureMonitoring
 import io.github.aifiltration.plugins.configureRouting
-import io.github.aifiltration.plugins.configureSecurity
-import io.github.aifiltration.plugins.configureSerialization
 
 fun main() {
 	embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
-	configureSecurity()
 	configureMonitoring()
-	configureSerialization()
 	configureRouting()
 }
