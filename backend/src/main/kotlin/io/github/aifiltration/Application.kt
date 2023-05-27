2b9354b15cb8b5ef21732db00130934426303b84
Ayfri <pierre.ayfri@gmail.com>
2023-05-28 01:58:29 +0200

    chore(back): Use ktor auto-reload on backend.

--- backend/src/main/kotlin/io/github/aifiltration/Application.kt
+++ backend/src/main/kotlin/io/github/aifiltration/Application.kt
@@ -7,7 +7,13 @@ import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-	embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
+	embeddedServer(
+		factory = CIO,
+		port = 8080,
+		host = "0.0.0.0",
+		module = Application::module,
+		watchPaths = listOf("backend"),
+	).start(wait = true)
 }
 
 fun Application.module() {
