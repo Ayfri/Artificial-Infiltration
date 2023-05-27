7294d944d49fa780f179cdd900b1702b492f5e35
Ayfri <pierre.ayfri@gmail.com>
2023-05-27 03:22:14 +0200

    fix(sql): Fix tables without primary keys.

--- backend/src/main/kotlin/io/github/aifiltration/models/Badge.kt
+++ backend/src/main/kotlin/io/github/aifiltration/models/Badge.kt
@@ -2,12 +2,15 @@ package io.github.aifiltration.models
 
 import org.komapper.annotation.KomapperColumn
 import org.komapper.annotation.KomapperEntity
+import org.komapper.annotation.KomapperId
 import org.komapper.annotation.KomapperTable
 
 @KomapperEntity
 @KomapperTable("badges")
 data class Badge(
 	@KomapperColumn("badge_id")
+	@KomapperId(virtual = true)
 	val id: Int,
+	@KomapperId(virtual = true)
 	val userId: Int,
 )
--- backend/src/main/kotlin/io/github/aifiltration/models/UserGame.kt
+++ backend/src/main/kotlin/io/github/aifiltration/models/UserGame.kt
@@ -1,11 +1,14 @@
 package io.github.aifiltration.models
 
 import org.komapper.annotation.KomapperEntity
+import org.komapper.annotation.KomapperId
 import org.komapper.annotation.KomapperTable
 
 @KomapperEntity
 @KomapperTable("user_games")
 data class UserGame(
-	val userId: Int,
+	@KomapperId(virtual = true)
 	val gameId: Int,
+	@KomapperId(virtual = true)
+	val userId: Int,
 )
